015ace8c1bcd0f400173fe605abb6caa526109e6
manojsuthar1412 <msuthar1412@gmail.com>
2022-12-09 17:56:04 +0530

    updated env file in frontend

--- server.js
+++ server.js
@@ -324,10 +324,10 @@ app.get("/upload", async (req, res) => {
   }, 50000);
 });
 
-app.use(express.static(path.join(__dirname, "build")));
+// app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`App running on PORT ${PORT}`));
